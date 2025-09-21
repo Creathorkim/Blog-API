9055338a69c75a24bc40f6e47670e38150303f5a
Mustakeem Ololade <mustakeemololade@gmail.com>
2025-09-21 08:11:57 +0100

    minimal changes

--- Api/server.js
+++ Api/server.js
@@ -1,8 +1,10 @@
-import authRouter from "./routes/route.js";
 import express from "express";
 import dotenv from "dotenv";
+import authRouter from "./routes/route.js";
 import cors from "cors";
 dotenv.config();
+
+
 const app = express();
 
 app.use(
@@ -16,7 +18,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use("/", authRouter);
 
-const PORT = process.env.PORT;
-app.listen(PORT, () => {
-  console.log(`App live at http://localhost:${PORT}/`);
+const PORT= process.env.PORT || 5000;
+app.listen(PORT , () => {
+  console.log(`App live at http://localhost:${PORT}`);
 });
