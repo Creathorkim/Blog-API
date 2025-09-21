12286c6f6d7b56f26e5c2c6d8d0f52fea512bd0a
Mustakeem Ololade <mustakeemololade@gmail.com>
2025-09-21 08:07:33 +0100

    Resolve CORS issue with API endpoints

--- Api/server.js
+++ Api/server.js
@@ -4,12 +4,16 @@ import authRouter from "./routes/route.js";
 import cors from "cors";
 dotenv.config();
 
-
 const app = express();
 
 app.use(
   cors({
-    origin: ["http://localhost:5173", "http://localhost:5174"],
+    origin: [
+      "http://localhost:5173",
+      "http://localhost:5174",
+      "https://blog-api-xpyf.vercel.app/",
+      "https://authorblog-api-7uhz.vercel.app/",
+    ],
     credentials: true,
   })
 );
@@ -18,7 +22,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use("/", authRouter);
 
-const PORT= process.env.PORT || 5000;
-app.listen(PORT , () => {
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
   console.log(`App live at http://localhost:${PORT}`);
 });
